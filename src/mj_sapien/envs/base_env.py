b6a15158b16ecc184fc774e580d327e1d3f1460f
Stone Tao <stonezt2019@gmail.com>
2026-04-21 17:03:12 -0700

    notes

--- src/mj_sapien/envs/base_env.py
+++ src/mj_sapien/envs/base_env.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from typing import NamedTuple
+from typing import Any, NamedTuple
 
 from mj_sapien.sim import backend
 
-
+import torch
 @dataclass(frozen=True)
 class BaseEnvCfg:
     num_envs: int = 1
@@ -19,6 +19,7 @@ class BaseEnv:
     def __init__(self, cfg: BaseEnvCfg):
         self.cfg = cfg
 
+    ### Scene management and instantiation functions ###
     def _load_scene(self):
         """
         Loads all scene rigid bodies, articulations etc.
@@ -31,24 +32,55 @@ class BaseEnv:
         and calling all relevant functions such as `_load_scene`
         """
         pass
+    
+    def _initialize_episode(self, env_idx: torch.Tensor):
+        """
+        Initialize the episode by setting the appropriate sim state for the environment
+        """
+        pass
 
+    ### Observables ###
     def get_obs(self):
         """
         Retrieve the current observation of the environment
         """
         pass
 
+    def evaluate(self) -> dict[str, Any]:
+        """
+        Evaluate the current state of the environment
+        """
+        return dict()
+
+    ### Gymnasium style lifecycle functions ###
     def reset(self, options: ResetOptions | None = None):
         """
         Reset the environment. This internally calls `_initialize_episode` to reset the sim state.
         """
-        pass
+        if options is not None:
+            if options["reconfigure"]:
+                self._reconfigure()
 
+        return self.get_obs(), self.evaluate()
+        
     def step(self):
         pass
 
     def render(self):
         """
         Render the current state of the environment
+        """
+        pass
+
+    ### Sim state management functions ###
+    def get_state_dict(self) -> dict[str, Any]:
+        """
+        Get the current state of the environment as a dictionary
+        """
+        pass
+
+    def set_state_dict(self, state_dict: dict[str, Any]):
+        """
+        Set the current state of the environment from a dictionary
         """
         pass
