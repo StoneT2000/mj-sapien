09ce275aff683e9694f2d0ea344f3bb574bf2491
Stone Tao <stonezt2019@gmail.com>
2026-06-05 13:56:31 -0700

    formatting

--- src/mj_sapien/agents/base_agent.py
+++ src/mj_sapien/agents/base_agent.py
@@ -1,20 +1,31 @@
 from dataclasses import dataclass
 
+from mj_sapien.agents.controllers.base_controller import BaseController
+
+
 @dataclass(frozen=True)
 class BaseAgentConfig:
     pass
 
 
 class BaseAgent:
-    """Base class for agents/robots. An agent/robot is an entity that can perform actions to interact with 
-    the environment. Users implementing their own agents/robots should inherit from this class.
+    """Base class for agents/robots. An agent/robot is an entity that can perform actions to
+    interact with the environment. Users implementing their own agents/robots should inherit
+    from this class.
     """
 
-    def __init__(self, cfg: BaseAgentConfig = BaseAgentConfig()):
+    def __init__(self, cfg: BaseAgentConfig):
         self.cfg = cfg
 
     def _load_agent(self):
         """
         Load a model of this agent into the environment
+        """
+        raise NotImplementedError()
+
+    @property
+    def controllers(self) -> dict[str, BaseController]:
+        """
+        Dictionary mapping controller ids to the controller objects
         """
         raise NotImplementedError()
--- src/mj_sapien/agents/controllers/base_controller.py
+++ src/mj_sapien/agents/controllers/base_controller.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
+
 import torch
 from gymnasium.spaces import Space
+
 
 @dataclass(frozen=True)
 class BaseControllerConfig:
     pass
+
 
 class BaseController:
     active_joint_indices: torch.Tensor
@@ -13,3 +16,16 @@ class BaseController:
 
     def __init__(self, cfg: BaseControllerConfig):
         self.cfg = cfg
+
+    @property
+    def device(self) -> torch.device:
+        """
+        The device the controller is on.
+        """
+        pass
+
+    def _preprocess_action(self, action: torch.Tensor) -> torch.Tensor:
+        """
+        Preprocess an action. By default the action is clipped to [-1, 1] and re-scaled to
+        the action space low, high bounds
+        """
